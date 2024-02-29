d29d69db68703b0c3add0322387910abf3fd1ee4
way2reyaj <way2reyaj1998@gmail.com>
2024-02-29 08:21:56 +0300

    ch

---
+++ index.js
@@ -0,0 +1,12 @@
+const personName = 'hchhc';
+let gee;
+
+
+const age = 90;
+const rating = 90.99;
+const isShow = true;
+const bee = false;
+const val = null;
+console.log(personName.repeat(5));
+console.log(personName.repeat(5));
+console.log(val);
